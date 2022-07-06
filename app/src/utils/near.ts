18fdae93cf21f93375c1a9d4bff30d40f29274e9
Oleg Yevik <olegyevik@Olegs-MacBook-Pro.local>
2022-07-06 21:14:36 +1200

    Initial setup

---
+++ app/src/utils/near.ts
@@ -0,0 +1,52 @@
+import environment from "./config";
+import { connect, Contract, keyStores, WalletConnection } from "near-api-js";
+import { formatNearAmount } from "near-api-js/lib/utils/format";
+
+const nearEnv = environment("testnet");
+
+declare global {
+  interface Window {
+    walletConnection?: any;
+    accountId?: any;
+    contract?: any;
+  }
+}
+
+export async function initializeContract() {
+  // @ts-ignore
+  const near = await connect({
+    keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+    ...nearEnv,
+  });
+
+  window.walletConnection = new WalletConnection(near, null);
+  window.accountId = window.walletConnection.getAccountId();
+  window.contract = new Contract(
+    window.walletConnection.account(),
+    nearEnv.contractName,
+    {
+      viewMethods: ["getProduct", "getProducts"],
+      changeMethods: ["buyProduct", "setProduct"],
+    }
+  );
+}
+
+export async function accountBalance() {
+  return formatNearAmount(
+    (await window.walletConnection.account().getAccountBalance()).total,
+    2
+  );
+}
+
+export async function getAccountId() {
+  return window.walletConnection.getAccountId();
+}
+
+export function login() {
+  window.walletConnection.requestSignIn(nearEnv.contractName);
+}
+
+export function logout() {
+  window.walletConnection.signOut();
+  window.location.reload();
+}
